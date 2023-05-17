9284500560c68cd63bf5b1046dbd3aa94f8967a0
FernandesNz <matheusferrodrigues@gmail.com>
2023-05-17 01:06:07 -0300

    renomeando

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def main_menu():
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    import chose_player
+                    import choose_player
                 if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
                     options()
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
